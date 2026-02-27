40315a1b3bbec96eca5807340f4cca314e61ea0a
spx333 <spx3@ya.ru>
2026-02-27 16:15:26 +0300

    добавил AuthViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Сергей Петров on 27.02.2026.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+        }
+    
+    
+    
+    
+}
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Сергей Петров on 27.02.2026.
+//
+
+import UIKit
+
+final class WebViewViewController: UIViewController {
+    
+}
